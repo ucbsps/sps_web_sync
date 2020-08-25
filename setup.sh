3e6093758653b08dfdc53459d5201610e40eaea7
Carter Turnbaugh <carterturn@ocf.berkeley.edu>
2020-08-24 17:05:34 -0700

    Fix setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-virtualenv -p python3 venv
+virtualenv -p python3.7 venv
 
-venv/bin/pip install --upgrade mariadb
+venv/bin/pip install --upgrade pymysql
 venv/bin/pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
