3752c467f6d36624f2a0bf957542d02ff3305f42
AdarshI <adarsh@adarshiyer.net>
2022-11-09 20:19:41 -0800

    fix the venv initialization

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-virtualenv -p python3.7 venv
+python3 -m venv venv
 
 venv/bin/pip install --upgrade pymysql
 venv/bin/pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
